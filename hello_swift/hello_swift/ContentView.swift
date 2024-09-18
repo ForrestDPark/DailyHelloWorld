dd4d25da700b4205978a287913ef0db83ca555f5
pulpilisory <pulpilisory@gmailcom>
2024-09-18 15:56:14 +0900

    pdg : R , swift update

---
+++ hello_swift/hello_swift/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  hello_swift
+//
+//  Created by 박동근 on 9/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("2024.09.18 Wed. PM 03:50 \n Hello,world!")
+            
+                 
+                
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hello_swift/hello_swift/hello_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_swiftApp.swift
+//  hello_swift
+//
+//  Created by 박동근 on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct hello_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
