e830761eae68e01c473a5e7894a1863addd2d34f
pulpilisory <pulpilisory@gmailcom>
2024-09-18 16:08:38 +0900

    pdg : java hello world update

---
+++ helloJava/helloJava/src/com/javalec/base/helloWorld.java
@@ -0,0 +1,10 @@
+package com.javalec.base;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2024.09.18 Wed. PM 04:08 hello world");
+	}
+
+}
