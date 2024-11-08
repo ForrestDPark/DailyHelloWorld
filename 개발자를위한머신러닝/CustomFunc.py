af829bd26b2da0a5755ba8e8c457b3b453b47c98
pulpilisory <pulpilisory@gmailcom>
2024-11-08 21:51:54 +0900

    pdg : 123

--- 개발자를위한머신러닝/CustomFunc.py
+++ 개발자를위한머신러닝/CustomFunc.py
@@ -55,7 +55,47 @@ class NLP:
             if word not in stopwords:
                 filtered_sentence =filtered_sentence + word + " "
         return filtered_sentence
+    
+    def tr_te_split(sentences,
+                labels,
+                training_size = 28000,
+                vocab_size =20000
+                ):
+        print(r_g("sentence training, testing split program module start"))
+        import tensorflow as tf
+        from tensorflow import keras
+        from tensorflow.keras.preprocessing.text import Tokenizer
+        from tensorflow.keras.preprocessing.sequence import pad_sequences
+        training_sentneces = sentences[0:training_size]
+        testing_sentences = sentences[training_size:]
+        training_labels = labels[0:training_size]
+        testing_labels = labels[training_size:]
         
+        max_length = 10
+        trunc_type = 'post'
+        padding_type = 'post'
+        oov_tok = "<OOV>"
+        
+        tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+        tokenizer.fit_on_texts(training_sentneces)
+        
+        word_index = tokenizer.word_index
+        
+        training_sequences = tokenizer.texts_to_sequences(training_sentneces)
+        training_padded = pad_sequences(training_sequences,maxlen=max_length,
+                                        padding=padding_type, truncating = trunc_type)
+        
+        testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+        testing_padded = pad_sequences(testing_sequences,maxlen=max_length,
+                                        padding=padding_type, truncating = trunc_type)
+        y(" training sequence 0")
+        print(training_sequences[0])
+        y(" training padded 0")
+        print(training_padded[0])
+        y("tokenizer word index")
+        print(tokenizer.word_index)
+        print(r_g("반환 : training_padded,training_labels, testing_padded, testing_labels"))
+        return training_padded,training_labels, testing_padded,testing_labels
 class DB:
     def __init__(self):
         pass
