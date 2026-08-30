a98b6ef8d58b75916655a55a388d300952f5704f
pulpilisory <pulpilisory@gmailcom>
2026-08-30 16:57:14 +0900

    Add Jiudi verse 20 full analysis

---
+++ 손자병법/copy_jiudi20_visuals.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+set -euo pipefail
+src=/Users/forrestdpark/.codex/generated_images/019f9ce9-41f0-7390-8614-772f885a9fbc
+dst=/Users/forrestdpark/Desktop/PDG/DailyHelloWorld_/손자병법/generated/jiudi20
+mkdir -p "$dst"
+cp "$src/exec-d578f771-1da7-404c-ab8f-2d36d9114851.png" "$dst/gaugamela_commanders.png"
+cp "$src/exec-ee7a0268-31f4-48d4-bfeb-452cb7110121.png" "$dst/gaugamela_soldiers_weapons_life.png"
+cp "$src/exec-63bb7383-cc29-4232-a3b2-958fd1d79a2b.png" "$dst/gaugamela_command_structure.png"
+cp "$src/exec-2205fe23-a422-4e39-8af2-7c4ade3c2c1e.png" "$dst/gaugamela_country_map.png"
+cp "$src/exec-11580690-10af-4e33-b39d-67c9527849a4.png" "$dst/gaugamela_strategy_map.png"
+cp "$src/exec-d3392e44-7f19-4ecb-bfb7-27dfff92b744.png" "$dst/gaugamela_sequence.png"
+cp "$src/exec-78ffafc7-a625-4e9b-a250-e3115ca4ea39.png" "$dst/hulao_commanders.png"
+cp "$src/exec-3c20fea3-9332-4c77-89fa-73d770af8fc7.png" "$dst/hulao_soldiers_weapons_life.png"
+cp "$src/exec-d1b681b0-875b-4404-932e-0dc3ed9aeccb.png" "$dst/hulao_command_structure.png"
+cp "$src/exec-165b78bc-4ce3-4d5a-aad7-fe87337ee338.png" "$dst/hulao_country_map.png"
+cp "$src/exec-e00b2cf7-d874-4311-ba72-77c8df551f24.png" "$dst/hulao_strategy_map.png"
+cp "$src/exec-38f79905-a5e5-4a35-b3ba-4f6829ee3ea9.png" "$dst/hulao_sequence.png"
